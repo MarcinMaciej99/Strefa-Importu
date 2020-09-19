b8e6c77570601c84b74d44f4842e54c534f76b9b
PawelKonie99 <63868700+PawelKonie99@users.noreply.github.com>
2020-09-19 15:01:40 +0200

    Header and start making main section

---
+++ src/components/Header/Header.js
@@ -0,0 +1,28 @@
+// import { Link } from "gatsby"
+// import PropTypes from "prop-types"
+import React, { useState } from "react";
+// import "../styles/styles.scss"
+import "./Header.scss";
+// import hamburger from "../images/bars-solid.svg"
+import logo from "../../images/logo.png";
+
+export const Header = () => {
+  const [open, setOpen] = useState(false);
+  return (
+    <header>
+      <div className="header">
+        <div className="header__links">
+          <img src={logo} alt="" className="header__logo" />
+          <div className="header__clickable-links">
+            <div className="header__clickable-links--calculator">
+              Kalkulator
+            </div>
+            <div className="header__clickable-links--link">Auta klientów</div>
+            <div className="header__clickable-links--link">Kontakt</div>
+            <div className="header__clickable-links--link">O nas</div>
+          </div>
+        </div>
+      </div>
+    </header>
+  );
+};
---
+++ src/components/MainSection/MainSection.js
@@ -0,0 +1,46 @@
+import React, { useEffect } from "react";
+// import Typical from "react-typical";
+import "./MainSection.scss";
+// import lottie from "lottie-web";
+
+export const MainSection = () => {
+  let animationContainer = React.createRef();
+
+  // useEffect(() => {
+  //   lottie.loadAnimation({
+  //     container: animationContainer.current,
+  //     animationData: animation,
+  //   })
+  // }, [])
+
+  return (
+    <main>
+      <div className="main-container">
+        <div className="main-section">
+          {/* <Typical
+            loop={Infinity}
+            wrapper="b"
+            steps={[
+              "Doświadczenie",
+              3000,
+              "Kreatywność",
+              3000,
+              "Zajawka",
+              3000,
+            ]} */}
+          {/* /> */}
+          <div className="main-section__description">
+            ... I wiele innych cech, wyróżnia nas sposród innych! Skontaktuj się
+            z nami a stworzymy twoją wymarzoną strone internetową.
+          </div>
+          <div className="main-section__buttons">
+            <button className="main-section__buttons--contact">Kontakt</button>
+            <button className="main-section__buttons--projects">Kontakt</button>
+          </div>
+        </div>
+      </div>
+    </main>
+  );
+};
+
+// export default MainSection;
