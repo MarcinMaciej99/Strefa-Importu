3b6da7f6d07fc7673ccfc0876595953df981c668
PawelKonie99 <63868700+PawelKonie99@users.noreply.github.com>
2020-09-19 16:14:45 +0200

    added acordian

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import React from "react";
 import "./App.scss";
 import { Header } from "./components/Header/Header";
 import { MainSection } from "./components/MainSection/MainSection";
+import Informations from "./components/Informations/Accordian";
 
 function App() {
   return (
     <div className="App">
       <Header />
       <MainSection />
+      <Informations />
     </div>
   );
 }
